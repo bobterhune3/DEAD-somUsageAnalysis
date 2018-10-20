74c04341c53afd76d0c0f7f19758f6883b860990
Bob Terhune <bobterhune3@gmail.com>
2018-10-20 08:48:01 -0700

    Checkpoint

--- Program.cs
+++ Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+..using somReporter.util.somUsageAnalysis;
+using somReporter.team;
 
-namespace somUsageAnaysys
+namespace somReporter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting...");
+
+            try
+            {
+                Program program = new Program();
+                program.initialize();
+            }
+            catch( FileNotFoundException fnfex)
+            {
+                Console.Out.WriteLine(fnfex.Message);
+            }
+
+        }
+
+        public void initialize() 
+        {
+
+
         }
     }
 }
